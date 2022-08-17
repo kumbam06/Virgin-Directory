8673da989e800625c4b33250042e2f95db119990
kumbam06 <kumbam.pradeepreddy@gmail.com>
2022-08-17 15:51:06 +0100

    roomtableviewcell

---
+++ Virgin Directory/View/RoomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RoomTableViewCell.swift
+//  Virgin Directory
+//
+//  Created by pradeep reddy kumbam on 14/08/22.
+//
+
+import UIKit
+
+class RoomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
