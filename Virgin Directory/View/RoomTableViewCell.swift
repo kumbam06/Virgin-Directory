1f068a6ec5be95d5f6c50a0b90706b5b20983bcf
kumbam06 <kumbam.pradeepreddy@gmail.com>
2022-08-17 15:51:47 +0100

    roomTVC

--- Virgin Directory/View/RoomTableViewCell.swift
+++ Virgin Directory/View/RoomTableViewCell.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class RoomTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var roomsAvailabilityLbl: UILabel!
+    @IBOutlet weak var roomsOccupancyLbl: UILabel!
+    @IBOutlet weak var roomsCreatedLbl: UILabel!
+    
+    public func configure(with roomsAvailability: Bool, roomOccupancy: Int, roomCreated: String){
+        
+        let availableText : String
+        
+        if roomsAvailability {
+           availableText = "Available"
+        }
+        else{
+            availableText = "Not Available"
+        }
+        
+        let date = Date()
+        let format = date.getFormattedDate(format: "yyyy-MM-dd HH:mm:ss")
+        
+        roomsAvailabilityLbl.text = "Room Availability : \(availableText)"
+        roomsOccupancyLbl.text = "Room Occupancy : \(roomOccupancy)"
+        roomsCreatedLbl.text = "Created on : \(format)"
+    }
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,4 +43,12 @@ class RoomTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+}
+
+extension Date {
+   func getFormattedDate(format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: self)
+    }
 }
